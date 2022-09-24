3b20c32ff1bd5aefb3c9022d8c7f1f6732f830dd
xkccoding <xkccoding@gmail.com>
2022-09-24 09:06:16 +0800

    ✨ feat: 增加左右点击箭头

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -6,7 +6,11 @@ import {
 	fetchNowPlaying,
 } from '../utils/moviedb.js';
 import Movie from './Movie';
-const Row = ({ title }) => {
+import {
+	BsFillArrowLeftCircleFill,
+	BsFillArrowRightCircleFill,
+} from 'react-icons/bs';
+const Row = ({ title, index }) => {
 	const mapRequest = {
 		UpComing: fetchUpcoming,
 		Popular: fetchPopMovie,
@@ -17,20 +21,41 @@ const Row = ({ title }) => {
 	useEffect(() => {
 		const fetchData = async () => {
 			const response = await mapRequest[title]();
-			console.log('response', response);
 			setMovies(response.results);
 		};
 		fetchData();
 	}, [title]);
+
+	const slideLeft = () => {
+		const slider = document.querySelector(`.slider${index}`);
+		slider.scrollLeft -= 500;
+	};
+	const slideRight = () => {
+		const slider = document.querySelector(`.slider${index}`);
+		slider.scrollLeft += 500;
+	};
 	return (
 		<div className='py-4'>
 			<h2 className='text-white text-3xl relative'>{title}</h2>
-			<div className='flex items-center relative'>
-				<div id='slider'>
+			<div className='flex items-center relative group'>
+				<BsFillArrowLeftCircleFill
+					className='bg-white absolute rounded-full z-10 cursor-pointer left-0 opacity-50 hover:opacity-100 hidden group-hover:block'
+					size={40}
+					onClick={slideLeft}
+				/>
+				<div
+					id='slider'
+					className={`slider${index} whitespace-nowrap overflow-x-scroll scroll-smooth`}
+				>
 					{movies.map((movie) => (
 						<Movie movie={movie} key={movie.id} />
 					))}
 				</div>
+				<BsFillArrowRightCircleFill
+					className='bg-white absolute rounded-full cursor-pointer right-0 opacity-50 hover:opacity-100 hidden group-hover:block'
+					size={40}
+					onClick={slideRight}
+				/>
 			</div>
 		</div>
 	);
